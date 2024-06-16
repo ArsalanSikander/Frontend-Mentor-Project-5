b5da44bf0a7e86860d5817c73d2b6ed6cd6fb5b4
ArsalanSikander <arsalansikander0216@gmail.com>
2024-06-16 17:56:11 +0500

    Upload local repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home'
+import Destination from './components/destination'
+import Crew from './components/crew'
+import Technology from './components/technology'
+import { Routes, Route, Link } from 'react-router-dom'
+import logo from './assets/shared/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div id='topNav' class='center'>
+
+        <div id='logoCont'>
+          <img src={logo} alt='the logo' />
+        </div>
+
+        <div id='lineCont'>
+          <p>some words</p>
+        </div>
+
+        <div id='navList' class='center'>
+          <ul id='navListInner' >
+            <li>
+              <Link to='/'>00 Home</Link>
+            </li>
+            <li>
+              <Link to='/destination'>01 Destination</Link>
+            </li>
+            <li>
+              <Link to='/crew'>02 Crew</Link>
+            </li>
+            <li>
+              <Link to='/technology'>03 Technology</Link>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/destination' element={<Destination />} />
+        <Route path='/crew' element={<Crew />} />
+        <Route path='/technology' element={<Technology />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,29 @@
+import './home.css';
+
+const Home = () => {
+    return (
+        <div id='cont'>
+            <div id='mainCont'>
+                <p id='introText'>So, you want to travel to</p>
+                <h1 id='bigText'>
+                    Space
+                </h1>
+                <p id='descText'>
+                    Let's face it; if you want to go to space, you might as well genuinely go to
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax
+                    because we'll give you a truly out of this world experience!
+                </p>
+            </div>
+
+
+            <div id='exploreCont'>
+                <h3 id='exploreText'>
+                    Explore
+                </h3>
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
